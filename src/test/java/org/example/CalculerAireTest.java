88f28400a708116ee37f4f29f01ac074629f5f07
MOURID Zakaria <zakariamourid142@gmail.com>
2024-01-25 23:16:20 +0100

    correction

--- src/test/java/org/example/CalculerAireTest.java
+++ src/test/java/org/example/CalculerAireTest.java
@@ -40,40 +40,20 @@ class CalculerAireTest {
     public void testAireWithDifferentFormes() {
         FormeFacade mockForme1 = mock(FormeFacade.class);
         FormeFacade mockForme2 = mock(FormeFacade.class);
+        FormeFacade carreMock = mock(FormeFacade.class);
 
         // Mocking the behavior of aire() method for the mocked objects
         when(mockForme1.aire()).thenReturn(7.0);
         when(mockForme2.aire()).thenReturn(12.0);
+        when(carreMock.aire()).thenReturn(4.0);
 
         CalculerAire calculerAire = new CalculerAire();
-        List<FormeFacade> formes = List.of(mockForme1, mockForme2);
+        List<FormeFacade> formes = List.of(mockForme1, mockForme2, carreMock);
 
         double result = calculerAire.aire(formes);
-        assertEquals(19.0, result, 0.0001); // Adjust the expected value based on your logic
+        assertEquals(23.0, result, 0.0001); // Adjust the expected value based on your logic
     }
-    @Test
-    public void testAireWithSquare() {
-        // Créer un mock pour la classe Carre
-        Rectangle rectangleMock = mock(Rectangle.class);
 
-        // Définir le comportement du mock pour la méthode aire() du carré
-        when(rectangleMock.aire()).thenReturn(4.0);
-
-        // Créer une instance de CalculerAire
-        CalculerAire calculerAire = new CalculerAire();
-
-        // Créer une liste de formes avec le carré mock
-        List<FormeFacade> formes = List.of(rectangleMock);
-
-        // Appeler la méthode aire() de CalculerAire avec la liste de formes
-        double result = calculerAire.aire(formes);
-
-        // Vérifier que l'appel à la méthode aire() du carré a été réalisé
-        verify(rectangleMock, times(1)).aire();
-
-        // Vérifier le résultat
-        assertEquals(4.0, result, 0.0001); // Adjust the expected value based on your logic
-    }
     @Test
     public void testAireWithCarreForm() {
         // Créer un mock pour une forme inconnue (une forme qui n'est pas définie dans votre application)
